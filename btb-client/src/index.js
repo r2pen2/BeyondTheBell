48ee9056ee79bc7040b9ec04ddac6501366a0502
r2pen2 <joedobbelaar@gmail.com>
2023-05-27 15:41:18 -0400

    Home Page Work + Themes

--- btb-client/src/index.js
+++ btb-client/src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { createTheme, NextUIProvider, } from "@nextui-org/react"
 
-const theme = createTheme({
-  type: "light", // it could be "light" or "dark"
-  theme: {
-    colors: {
-      btbOrange: '#F05A21',
-      textSecondary: "#b9b9b9",
-    },
-    space: {},
-    fonts: {}
-  }
-})
+import { theme } from "./assets/style/theme";
+
+import { NextUIProvider, } from "@nextui-org/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
