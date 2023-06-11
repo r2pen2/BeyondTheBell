263dcecbb614f90612aaa16a2d5a4e1a25e59977
r2pen2 <joedobbelaar@gmail.com>
2023-06-10 22:01:45 -0400

    MemberModal

--- btb-client/src/routes/About.jsx
+++ btb-client/src/routes/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { Button, Card, Spacer, Text, } from "@nextui-org/react";
+import { Button, Card, Modal, Text, } from "@nextui-org/react";
 
 import "../assets/style/about.css"
 import { PageHeader, OrangeBar } from '../components/Bar';
@@ -19,12 +19,109 @@ import rachelGarcia from "../assets/images/staff/rachelGarcia.jpg"
 import { FormModal } from "../components/Forms";
 import { btbOrange } from '../assets/style/colors';
 
+
+
+const teamMembers = [
+  {
+    order: 1,
+    name: "Jessica Reynolds",
+    position: "English Tutor",
+    image: jessicaReynolds,
+    bio: "Jessica is an English tutor with a Master's in English Literature. She is passionate about teaching and has been working with students for 7 years. Her focus is on improving students' writing skills and helping them appreciate literature. Jessica believes that good communication skills are essential for success in any field. Outside of the tutoring center, she is an avid reader and loves to write short stories."
+  },
+  {
+    order: 2,
+    name: "David Kim",
+    position: "Science Tutor",
+    image: davidKim,
+    bio: "David is a Science Tutor, specializing in Physics and Chemistry. He has a Master's degree in Chemical Engineering. His scientific background allows him to make science engaging and relatable to students. David has a talent for breaking down complicated concepts into easy-to-understand language. When he’s not teaching, David enjoys tinkering with electronics and playing the guitar."
+  },
+  {
+    order: 6,
+    name: "Lisa Martinez",
+    position: "Administrative Assistant",
+    image: lisaMartinez,
+    bio: "Lisa is the Administrative Assistant and a crucial part of the team. She has been with BTB for 3 years, ensuring that operations run efficiently. Her organizational skills and attention to detail keep the center in top shape. Lisa is usually the first point of contact for parents and students, and she’s known for her warm and friendly demeanor. In her leisure time, she enjoys painting and taking nature walks."
+  },
+  {
+    order: 5,
+    name: "Matthew Broderick",
+    position: "Math Tutor",
+    image: matthewBroderick,
+    bio: "Matthew Broderick, renowned for his illustrious career in acting, has joined Beyond The Bell as a Math Tutor. Surprising to many, Matthew has a deep-seated love for mathematics and decided to make a career change to pursue this passion. With his charisma and ability to perform, he makes learning math an incredibly engaging experience for students. He believes in making a difference in the lives of the youth and takes immense joy in witnessing their progress. When not tutoring, he sometimes reminisces about his acting days and shares exciting stories with students and staff."
+  },
+  {
+    order: 4,
+    name: "Jason Brown",
+    position: "SAT/ACT Prep Tutor",
+    image: jasonBrown,
+    bio: "Jason is specialized in SAT and ACT prep, and has been a tutor for over 6 years. He has a degree in Psychology, which he utilizes to understand students' learning patterns and adapt his teaching methods. Jason is passionate about equipping students with the strategies and confidence needed to excel in standardized tests. He is also a basketball enthusiast and coaches a local youth team in his spare time."
+  },
+  {
+    order: 3,
+    name: "Rachel Garcia",
+    position: "Academic Advisor",
+    image: rachelGarcia,
+    bio: "Rachel serves as an Academic Advisor at the center. With a background in Counseling, she plays a vital role in helping students plan their educational path. She believes that proper guidance is essential for students to make informed decisions regarding their academic and career goals. Rachel is also an advocate for mental health and conducts workshops for stress management."
+  }
+]
+
 export default function About() {
 
   const [modalOpen, setModalOpen] = useState(false);
 
+  const [currentTeamMember, setCurrentTeamMember] = useState({
+    order: -1,
+    name: null,
+    position: null,
+    image: null,
+    bio: null,
+  })
+
+  const [teamMemberModalOpen, setTeamMemberModalOpen] = useState(false);
+
   return (
     <div className="d-flex flex-column">
+      <Modal 
+        closeButton
+        width="75vw"
+        open={teamMemberModalOpen}
+        blur
+        onClose={() => setTeamMemberModalOpen(false)}
+      >
+        <Modal.Body>
+          <div className="container-fluid">
+            <div className="row d-flex flex-row align-items-center justify-content-center">
+              <div className="col-lg-4 col-md-12">
+                <img src={currentTeamMember.image} alt={currentTeamMember.name} className="img-shadow"/>
+              </div>
+              <div className="col-lg-8 p-3 col-md-12 d-flex flex-column justify-content-center">
+                <div className="w-100 d-flex flex-row gap-2 justify-content-center">
+                  <Text size="$lg" css={{fontWeight: "bold"}} >
+                    {currentTeamMember.name}
+                  </Text>
+                  <Text>
+                    —
+                  </Text>
+                  <Text>
+                    {currentTeamMember.position}
+                  </Text>
+                </div>
+                <Text align="center">
+                  {currentTeamMember.bio}
+                </Text>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="d-flex flex-row align-items-center justify-content-center w-100">
+            <Button auto flat color="error" onPress={() => setTeamMemberModalOpen(false)} >
+                Close
+            </Button>
+          </div>
+        </Modal.Footer>
+      </Modal>
       <PageHeader 
         text="About Beyond the Bell"
         sections={[
@@ -141,81 +238,55 @@ export default function About() {
       <FormModal open={modalOpen} setOpen={setModalOpen}/>
     </div>
   )
-}
 
-const teamMembers = [
-  {
-    order: 1,
-    name: "Jessica Reynolds",
-    position: "English Tutor",
-    image: jessicaReynolds,
-    bio: "Jessica is an English tutor with a Master's in English Literature. She is passionate about teaching and has been working with students for 7 years. Her focus is on improving students' writing skills and helping them appreciate literature. Jessica believes that good communication skills are essential for success in any field. Outside of the tutoring center, she is an avid reader and loves to write short stories."
-  },
-  {
-    order: 2,
-    name: "David Kim",
-    position: "Science Tutor",
-    image: davidKim,
-    bio: "David is a Science Tutor, specializing in Physics and Chemistry. He has a Master's degree in Chemical Engineering. His scientific background allows him to make science engaging and relatable to students. David has a talent for breaking down complicated concepts into easy-to-understand language. When he’s not teaching, David enjoys tinkering with electronics and playing the guitar."
-  },
-  {
-    order: 6,
-    name: "Lisa Martinez",
-    position: "Administrative Assistant",
-    image: lisaMartinez,
-    bio: "Lisa is the Administrative Assistant and a crucial part of the team. She has been with the center for 3 years, ensuring that operations run efficiently. Her organizational skills and attention to detail keep the center in top shape. Lisa is usually the first point of contact for parents and students, and she’s known for her warm and friendly demeanor. In her leisure time, she enjoys painting and taking nature walks."
-  },
-  {
-    order: 5,
-    name: "Matthew Broderick",
-    position: "Math Tutor",
-    image: matthewBroderick,
-    bio: "Matthew Broderick, renowned for his illustrious career in acting, has joined Beyond The Bell as a Math Tutor. Surprising to many, Matthew has a deep-seated love for mathematics and decided to make a career change to pursue this passion. With his charisma and ability to perform, he makes learning math an incredibly engaging experience for students. He believes in making a difference in the lives of the youth and takes immense joy in witnessing their progress. When not tutoring, he sometimes reminisces about his acting days and shares exciting stories with students and staff."
-  },
-  {
-    order: 4,
-    name: "Jason Brown",
-    position: "SAT/ACT Prep Tutor",
-    image: jasonBrown,
-    bio: "Jason is specialized in SAT and ACT prep, and has been a tutor for over 6 years. He has a degree in Psychology, which he utilizes to understand students' learning patterns and adapt his teaching methods. Jason is passionate about equipping students with the strategies and confidence needed to excel in standardized tests. He is also a basketball enthusiast and coaches a local youth team in his spare time."
-  },
-  {
-    order: 3,
-    name: "Rachel Garcia",
-    position: "Academic Advisor",
-    image: rachelGarcia,
-    bio: "Rachel serves as an Academic Advisor at the center. With a background in Counseling, she plays a vital role in helping students plan their educational path. She believes that proper guidance is essential for students to make informed decisions regarding their academic and career goals. Rachel is also an advocate for mental health and conducts workshops for stress management."
+  function renderTeam() {
+
+    const sortedTeamMembers = teamMembers.sort((a, b) => a.order - b.order);
+  
+    return sortedTeamMembers.map((teamMember, index) => {
+  
+      function handleCardClick() {
+        setCurrentTeamMember(teamMember);
+        setTeamMemberModalOpen(true);
+      }
+  
+      return (
+        <div className="col-xxl-3 col-xl-4 col-md-6 col-sm-12" key={index}>
+          <Card 
+            isHoverable 
+            isPressable 
+            className="p-3 m-2 d-flex flex-column align-items-center"
+            onPress={handleCardClick}
+          >
+            <Card.Body className="d-flex flex-column gap-2 align-items-center w-100 justify-content-center">
+              <img src={teamMember.image} alt={teamMember.name} className="img-shadow img-round" style={{width: "10rem", height: "10rem"}}/>
+              <div className="w-100 d-md-none d-lg-flex flex-row gap-2 justify-content-center">
+                <Text size="$lg" css={{fontWeight: "bold"}} >
+                  {teamMember.name}
+                </Text>
+                <Text>
+                  —
+                </Text>
+                <Text>
+                  {teamMember.position}
+                </Text>
+              </div>
+              <div className="w-100 d-md-flex d-lg-none flex-column align-items-center">
+                <Text size="$lg" css={{fontWeight: "bold"}} >
+                  {teamMember.name}
+                </Text>
+                <Text>
+                  {teamMember.position}
+                </Text>
+              </div>
+            </Card.Body>
+              <Button bordered onClick={handleCardClick}>
+                Read More
+              </Button>
+          </Card>
+        </div>
+      )
+    })
   }
-]
-
-function renderTeam() {
-
-  const sortedTeamMembers = teamMembers.sort((a, b) => a.order - b.order);
-
-  return sortedTeamMembers.map((teamMember, index) => {
-    return (
-      <Card 
-        isHoverable 
-        isPressable 
-        className="m-2 p-3 d-flex flex-column align-items-center col-xxl-3 col-xl-4 col-md-6 col-sm-12"
-      >
-        <Card.Body className="d-flex flex-row gap-4 align-items-center w-100 justify-content-center">
-          <img src={teamMember.image} alt={teamMember.name} className="img-shadow img-round" style={{width: "10rem", height: "10rem"}}/>
-          <div className="w-100 d-flex flex-column align-items-center">
-            <Text size="$lg" b >
-              {teamMember.name}
-            </Text>
-            <Text>
-              {teamMember.position}
-            </Text>
-          </div>
-        </Card.Body>
-        <Card.Footer className="w-100 d-flex flex-row justify-content-center">
-          <Button bordered>
-            Read More
-          </Button>
-        </Card.Footer>
-      </Card>
-    )
-  })
 }
+
