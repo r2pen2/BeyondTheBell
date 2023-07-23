7e29785df2e159b3a91e4841e141e995b2f0bab4
r2pen2 <joedobbelaar@gmail.com>
2023-07-23 19:22:09 -0400

    more details on services

--- btb-client/src/routes/Services.jsx
+++ btb-client/src/routes/Services.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useState } from 'react'
 
-import { Card, Text } from "@nextui-org/react";
+import { Card, Divider, Text } from "@nextui-org/react";
 
 import "../assets/style/services.css"
 import { btbOrange } from '../assets/style/colors';
 import CheckIcon from '@mui/icons-material/Check';
 
 import { ScheduleBar } from '../components/Forms';
-import { WLHeader } from '../libraries/Web-Legos/components/Text';
+import { WLHeader, WLText } from '../libraries/Web-Legos/components/Text';
 import { CurrentUserContext } from '../App';
 import { WLResponsiveSectionEditable, WLSpinnerPage } from '../libraries/Web-Legos/components/Layout';
 import { ServicesBlockHeader } from '../components/Bar';
@@ -32,37 +32,13 @@ export default function Services() {
       <ServicesBlockHeader />
       <WLResponsiveSectionEditable 
         setLoaded={setAfterSchoolLoaded}
-        firestoreId="after-school" 
+        firestoreId="social-skills" 
         editable={userCanEditText}
         bottomContent={
-          <div className="row">
-            <div className="col px-xl-5 py-5">
-              <div className="container-fluid">
-                <WLHeader headerLevel={2} color="primary" firestoreId="sign-up-header" editable={userCanEditText}/>
-                <div className="row">
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      1 on 1 tutoring in all academic subjects including math, reading, science, and social studies
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Tutoring in executive functioning, study skills, social behavior mapping or any area that requires individual instruction
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Educators provide data confirming your child’s progress and participation
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Regular communication with parents and teachers (if appropriate)
-                    </SignUpReason>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="d-flex flex-row align-items-center justify-content-center" style={{maxWidth: 1400}}>
+            <WLHeader headerLevel={2} color="primary" firestoreId="social-skills-details-header" editable={userCanEditText}/>
+            <Divider />
+            <WLText editable={userCanEditText} firestoreId="social-skills-details" />
           </div>
         }
       />
@@ -72,29 +48,10 @@ export default function Services() {
         firestoreId="1-on-1-tutoring" 
         editable={userCanEditText} 
         bottomContent={  
-          <div className="row">
-            <div className="col px-xl-5 py-5">
-              <div className="container-fluid">              
-                <WLHeader headerLevel={2} color="primary" firestoreId="sign-up-header" editable={userCanEditText}/>
-                <div className="row">
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Homework support from experienced educators and certified teachers
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      A “chillaxing” zone (with hammock swings, bean bags, fidgets, noise cancelling headphones, Chromebook laptops and iPads to access Google Classroom and other resources)
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Study spots (including cozy study carrels, comfy chairs, the work bar, and adjustable height project tables)
-                    </SignUpReason>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="d-flex flex-row align-items-center justify-content-center" style={{maxWidth: 1400}}>
+            <WLHeader headerLevel={2} color="primary" firestoreId="1-on-1-tutoring-details-header" editable={userCanEditText}/>
+            <Divider />
+            <WLText editable={userCanEditText} firestoreId="1-on-1-tutoring-details" />
           </div>
         }
       />
@@ -104,64 +61,10 @@ export default function Services() {
         firestoreId="wilson-tutoring" 
         editable={userCanEditText}
         bottomContent={
-          <div className="row">
-            <div className="col px-xl-5 py-5">
-              <div className="container-fluid">
-                <WLHeader headerLevel={2} color="primary" firestoreId="sign-up-header" editable={userCanEditText}/>
-                <div className="row">
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Word structure, in depth, for automatic decoding and spelling
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Word recognition and spelling of high frequency words, including irregular words
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Word-learning skills
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Sentence-level text reading with ease, expression, and understanding
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Listening comprehension with age-appropriate narrative and informational text
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Reading comprehension with narrative and expository text of increasing levels of difficulty
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Narrative and informational text structures
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Organization of information for oral or written expression
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                      Proofreading skills
-                    </SignUpReason>
-                  </div>
-                  <div className="col-xxl-4 col-xl-6 col-lg-12">
-                    <SignUpReason>
-                    Self-monitoring for word recognition accuracy and comprehension
-                    </SignUpReason>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="d-flex flex-row align-items-center justify-content-center" style={{maxWidth: 1400}}>
+            <WLHeader headerLevel={2} color="primary" firestoreId="wilson-tutoring-details-header" editable={userCanEditText}/>
+            <Divider />
+            <WLText editable={userCanEditText} firestoreId="wilson-tutoring-details" />
           </div>
         }
       />
