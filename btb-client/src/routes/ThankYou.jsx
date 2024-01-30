a5eaef742a69921d5babdb3ba418f315e8dc5b54
Joe Dobbelaar <joedobbelaar@gmail.com>
2024-01-30 14:53:33 -0500

    CONFETTI!!!

--- btb-client/src/routes/ThankYou.jsx
+++ btb-client/src/routes/ThankYou.jsx
@@ -8,8 +8,12 @@ import { WLImage } from '../libraries/Web-Legos/components/Images';
 import { ThankYouBlockHeader } from '../components/Bar';
 import { useEffect } from 'react';
 
+import Confetti from 'react-confetti'
+// import useWindowSize from 'react-use/lib/useWindowSize'
+
 export default function ThankYou() {
 
+  // const { width, height } = useWindowSize()
 
   const [thankYouTextLoaded, setThankYouTextLoaded] = useState(false);
 
@@ -25,6 +29,10 @@ export default function ThankYou() {
 
   return (
     <WLSpinnerPage itemsCentered dependencies={[thankYouTextLoaded]}>
+      <Confetti
+        numberOfPieces={500}
+        recycle={false}
+      />
       <ThankYouBlockHeader />
       <section className="d-flex flex-column align-items-center justify-content-center m-5 gap-2">
         <WLText firestoreId="thank-you" editable={userCanEditText} setLoaded={setThankYouTextLoaded}/>
